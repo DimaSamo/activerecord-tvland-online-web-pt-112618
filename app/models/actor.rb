f053ba9e7104839ea323a8c413c7472899b55722
Dmitri Samotessov <dimasamo96@gmail.com>
2019-03-04 04:15:43 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,4 +6,10 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
 
+  def list_roles
+    self.characters.each do |character|
+      puts "#{character.role}"
+    end
+  end
+
 end
